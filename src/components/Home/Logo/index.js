b4a3b2ab5c71880154f84224890f72d7655b31be
Anirudh Ramesh <45203642+Rudha1@users.noreply.github.com>
2022-08-24 12:30:26 -0400

    updated logo

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,60 +1,63 @@
 import './index.scss'
-import LogoS from '../../../assets/images/logo-a.png'
+import LogoA from '../../../assets/images/logo-a.png'
 import { useEffect, useRef } from 'react'
+// import DrawSVGPlugin from 'gsap/DrawSVGPlugin'
+// import gsap from 'gsap'
 
-const Logo = () => {
-    const bgRef = useRef();
-    const tl = useRef(gsap.timeline());
-    const outlineLogoRef = useRef();
-    const solidLogoRef = useRef();
+const Logo = () => (
+    // const bgRef = useRef();
+    // // const tl = useRef(gsap.timeline());
+    // // const outlineLogoRef = useRef();
+    // const solidLogoRef = useRef();
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
+    // useEffect(() => {
+    //     // gsap.registerPlugin(DrawSVGPlugin)
 
-        tl.current.to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
+    //     tl.current.to(bgRef.current, {
+    //         duration: 1,
+    //         opacity: 1,
+    //     })
+    //     .from(outlineLogoRef.current, {
+    //         drawSVG: 0,
+    //         duration: 20,
+    //     })
 
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
-    return (
-        <div className = "logo-container" ref={bgRef}>
-            <img ref = {solidLogoRef} className="solid-logo" src={LogoS} alt="A" />
-            <svg 
+    //     gsap.fromTo(
+    //         solidLogoRef.current,
+    //         {
+    //             opacity: 0,
+    //         },
+    //         {
+    //             opacity: 1,
+    //             delay: 4,
+    //             duration: 4,
+    //         }
+    //     )
+    // }, [])
+    // return (
+        <div className = "logo-container">
+            <img className="solid-logo" src={LogoA} alt="A" />
+            {/* <svg 
                 width = "559pt"
                 height = "897pt"
                 version = "1.0"
                 viewBox = "0 0 559 897"
                 xmlns = "http://www.w3.org/2000/svg"
-            >
-            <g
+            > */}
+            {/* <g
                 className="svg-container"
                 transform= "translate(0 457) scale(.1 -.1)"
                 fill = "none"
-            >
-                <path 
+            > */}
+                {/* <path 
                 ref={outlineLogoRef}
-                d=""/>
-		</g>
-        
-            </svg>
+                d=""/> */}
+		{/* </g> */}
+        {/* ref = {solidLogoRef}  */}
+        {/* ref={bgRef} */}
+            {/* </svg> */}
             </div>
-    )
-}
+   // )
+)
 
 export default Logo
